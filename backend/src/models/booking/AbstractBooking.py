fb1e4732568626ad6725b194e7d96b37f78c41bd
ThakerJayam <jayam.thaker@gmail.com>
2022-05-01 01:50:40 -0700

    skeleton for booking

---
+++ backend/src/models/booking/AbstractBooking.py
@@ -0,0 +1,20 @@
+
+from abc import ABCMeta,ABC, abstractmethod
+from typing import List
+from src import db
+from src.models.user.AbtractUser import AbtractUser
+from src.models.room.AbstractRoom import AbstractRoom
+
+database = db.get_instance()
+
+class AbstractBooking(database.Model):
+    __abstract__ = True
+
+    def setCustomer(self,user:AbtractUser):
+        self.user = user
+
+    def setRoom(self,room:AbstractRoom):
+        self.room = room
+
+    def setInterval(self,start,end):
+        pass
