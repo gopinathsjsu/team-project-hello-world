473a8e44ce8576e8bb151c908e552d351a05f096
ThakerJayam <jayam.thaker@gmail.com>
2022-05-01 01:21:33 -0700

    structure change

--- backend/src/models/hotel/AbstractHotel.py
+++ backend/src/models/hotel/AbstractHotel.py
@@ -2,15 +2,19 @@ from src.models.room.AbstractRoom import AbstractRoom
 from abc import ABCMeta,ABC, abstractmethod
 from typing import List
 from src import db
+from datetime import date
 
 database = db.get_instance()
 
 
 class AbstractHotel(database.Model):
     __abstract__ = True
-    @abstractmethod
-    def getAvailibilityOn(start,end) -> List[AbstractRoom]:
-        pass
+    def getAvailibilityOn(self,start: date,end:date) -> List[AbstractRoom]:
+        available_rooms = []
+        for r in self.rooms:
+            if  r.isAvailableFor(start,end):
+                available_rooms.append(r)
+        return available_rooms
     
     @abstractmethod
     def getPrice(room: AbstractRoom,start,end,Amenities) -> float:
