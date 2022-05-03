76c499c6e68e602087057f4619ce19dd5c1952fa
Pratyush Sharma <pratyushsharma@outlook.in>
2022-05-02 22:52:43 -0700

    Modified user and added method

--- backend/src/models/user/ModerUser.py
+++ backend/src/models/user/ModerUser.py
@@ -1,18 +1,35 @@
 from abc import ABC
 from src.models.user.AbtractUser import AbstractUser
+from src.models.booking.AbstractBooking import AbstractBooking
+from src.models.hotel.ModelHotel import ModelHotel
 from typing import List
 from src import db, app
 
-database = db.get_instance()
+# Dummy data to test
+name = 'Pratyush'
+email = 'pratyush@sjsu.com'
+payment_info = "1234"
+layalty_points = 0
+bookings = []
 
-@app.route('/')
-def hello_world():
-        return "This is User class"
+class ModelUser(AbstractUser):
+    
+    def __init__(self, name, email, payment_info, ) -> None:
+        super().__init__()
+        self.name = name
+        self.email = email
+        self.payment_info = payment_info
 
-class ModelUser(database.Model):
-    id = database.Column(database.Integer, primary_key=True)
     def getHotels(start, end, location):
-        return
+        hotels = []
+        hotels = ModelHotel.query.filter_by(ModelHotel.location.like(location))
+
+        available_hotels = []
+        # for hotel in hotels:
+        #     is_available = hotel.getAvailibilityOn(start, end)
+        #     if(is_available):
+        #         available_hotels.append(hotel)
+        return available_hotels
 
     def bookHotel(room, start, end):
         return
