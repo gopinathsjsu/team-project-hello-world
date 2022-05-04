4e85094430bb60084cf5d31d35262bb17d221f11
Pratyush Sharma <pratyushsharma@outlook.in>
2022-05-03 20:05:33 -0700

    Added database conn to user

--- backend/src/models/user/ModerUser.py
+++ backend/src/models/user/ModerUser.py
@@ -1,35 +1,50 @@
 from abc import ABC
+from enum import unique
 from src.models.user.AbtractUser import AbstractUser
 from src.models.booking.AbstractBooking import AbstractBooking
-from src.models.hotel.ModelHotel import ModelHotel
+# from src.models.hotel.ModelHotel import ModelHotel
 from typing import List
-from src import db, app
+from src.db import db
 
 # Dummy data to test
-name = 'Pratyush'
-email = 'pratyush@sjsu.com'
-payment_info = "1234"
-layalty_points = 0
-bookings = []
+# name = 'Pratyush'
+# email = 'pratyush@sjsu.com'
+# payment_info = "1234"
+# layalty_points = 0
+# bookings = []
 
-class ModelUser(AbstractUser):
+class ModelUser(db.Model):
+
+    __tablename__ = "User"
+
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    email = db.Column(db.String(70), unique=True, nullable=False)
+    payment_info = db.Column(db.String(16), unique=True, nullable=True)
+    layalty_points = db.Column(db.Integer, unique=False, nullable=True)
+    bookings = db.Column(db.String(100), unique=False, nullable=True)
     
-    def __init__(self, name, email, payment_info, ) -> None:
+    def __init__(self, name, email, payment_info) -> None:
         super().__init__()
         self.name = name
         self.email = email
         self.payment_info = payment_info
 
-    def getHotels(start, end, location):
-        hotels = []
-        hotels = ModelHotel.query.filter_by(ModelHotel.location.like(location))
+    def dummy():
+        dummy_data = ModelUser('Pratyush', 'pratyush@sharma.com', '123456')
+        db.session.add(dummy_data)
+        db.session.commit()
 
-        available_hotels = []
+    def getHotels(start, end, location):
+        # hotels = []
+        # hotels = ModelHotel.query.filter_by(ModelHotel.location.like(location))
+
+        # available_hotels = []
         # for hotel in hotels:
         #     is_available = hotel.getAvailibilityOn(start, end)
         #     if(is_available):
         #         available_hotels.append(hotel)
-        return available_hotels
+        # return available_hotels
+        return
 
     def bookHotel(room, start, end):
         return
