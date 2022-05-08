fe3a02cc61cde6354150cc827c1088a1eca61ed0
ThakerJayam <jayam.thaker@gmail.com>
2022-05-07 18:34:05 -0700

    added ModelBooking

---
+++ backend/src/models/booking/ModelBooking.py
@@ -0,0 +1,10 @@
+from  src.models.booking.AbstractBooking import AbstractBooking
+from src.db import db as database
+
+
+class ModelBooking(AbstractBooking):
+    id = database.Column(database.Integer, primary_key=True)
+    start_date =  database.Column(database.DateTime)
+    end_date =  database.Column(database.DateTime)
+    room_id = database.Column(database.Integer, database.ForeignKey('room.id'),nullable=False)
+    user_id = database.Column(database.Integer, database.ForeignKey('User.id'),nullable=False)
