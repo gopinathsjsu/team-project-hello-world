7b7ca4b7636defea9de48261767175ed7bd028b7
Pratyush Sharma <pratyushsharma@outlook.in>
2022-05-09 15:08:23 -0700

    Add support for user login

--- backend/src/models/user/AbtractUser.py
+++ backend/src/models/user/AbtractUser.py
@@ -20,6 +20,24 @@ class AbstractUser(ABC):
             }
         return user_dict
 
+    def get_user_by_email_password(email, password):
+        user = userServices.get_user_by_email_password(email, password)
+        user_dict = {
+            'id': user.id,
+            'firstName': user.first_name,
+            'lastName': user.last_name,
+            'email': user.email,
+            'phoneNumber': user.phone_number,
+            'address': user.address,
+            'city': user.city,
+            'state': user.state,
+            'country': user.country,
+            'zip': user.zip,
+            'loyalty_points': user.loyalty_points
+            }
+        return user_dict
+
+
     def delete_user(id):
         userServices.deleteUser(id)
 
