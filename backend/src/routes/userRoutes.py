959b527454e7e7c9ef5bc946c5028d31db0a1f94
Pratyush Sharma <pratyushsharma@outlook.in>
2022-05-11 09:20:42 -0700

    Proper routing in user routes

--- backend/src/routes/userRoutes.py
+++ backend/src/routes/userRoutes.py
@@ -6,20 +6,16 @@ from src.models.user.AbstractUser import AbstractUser
 from src import app
 from flask_cors import CORS, cross_origin
 
-# user = Blueprint("user", __name__, url_prefix='/user')
+user = Blueprint("user", __name__, url_prefix='/user')
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-@app.route("/", methods=["GET"])
-def hello():
-    return "Hello"
-
-@app.route("/user/<user_id>",methods=["GET"])
+@user.route("/<user_id>",methods=["GET"])
 @cross_origin() 
 def get_user_details(user_id):
     response = AbstractUser.get_user_details(user_id)
     return response
 
-@app.route("/user/register",methods=["POST"])
+@user.route("/register",methods=["POST"])
 @cross_origin()
 def user_registration():
     if request.method == "POST":
@@ -28,7 +24,7 @@ def user_registration():
         user = AbstractUser.get_user_details(res.id)
         return user
 
-@app.route("/user/login",methods=["POST"])
+@app.route("/login",methods=["POST"])
 def login():
     user_data = request.get_json()
     data = userServices.login(user_data["email"],user_data["password"])
@@ -40,29 +36,29 @@ def login():
         resp = {"response":{"email" : data.email},"token":token}
         return resp
 
-@app.route("/user/dummy",methods=["POST"])
+@user.route("/dummy",methods=["POST"])
 def dummy():
     req =request.get_json()
     print(req["token"])
     x = userServices.validate_customer(req["token"])
     return str(x)
 
-@app.route("/<user_id>/delete", methods=["DELETE"])
+@user.route("/<user_id>/delete", methods=["DELETE"])
 def delete_user(user_id):
     AbstractUser.delete_user(user_id)
 
-@app.route("/hotels/<start>/<end>/<location>", methods=["GET"])
+@user.route("/hotels/<start>/<end>/<location>", methods=["GET"])
 @cross_origin()
 def get_hotels(start, end, location):
     hotels = AbstractUser.get_hotels(start, end, location)
     return hotels
 
-@app.route("/book/<room>/<start>/<end>",methods=["GET"])
+@user.route("/book/<room>/<start>/<end>",methods=["GET"])
 @cross_origin()
 def book_hotel(room, start, end):
     AbstractUser.book_hotel(room, start, end)
 
-@app.route("/booking/cancel",methods=["POST"])
+@user.route("/booking/cancel",methods=["POST"])
 @cross_origin()
 def cancel_booking():
     if request.method == "POST":
