c8245d906d3122dc3061189dd775cee444beae10
Ishan Malkan <ishanmalkan@gmail.com>
2022-05-11 23:29:39 -0700

    merge changes

--- ui/src/pages/auth/Login.js
+++ ui/src/pages/auth/Login.js
@@ -1,63 +1,79 @@
-import React, { useState } from 'react';
-import getLinks from '../../common/helper/links';
-import API from '../../common/helper/api';
-import { NavLink, Redirect, useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import getLinks from "../../common/helper/links";
+import API from "../../common/helper/api";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Login() {
-    let links = getLinks();
-    const [loginError, setLoginError] = useState(null);
-    const [formData, setFormData] = useState({
-        email: 'jayam.thaker@gmail.com',
-        password: 'a123',
-    })
+  let links = getLinks();
+  const [loginError, setLoginError] = useState(null);
+  const [formData, setFormData] = useState({
+    email: "jayam.thaker@gmail.com",
+    password: "a123",
+  });
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    function login(e) {
-        e.preventDefault();
-        let body = { ...formData }
+  function login(e) {
+    e.preventDefault();
+    let body = { ...formData };
 
-        API({
-            callURL: links.login,
-            callMethod: "POST",
-            bodyData: body,
-            callBack: (res) => {
-                if (res.status) {
-                    localStorage.setItem("user", JSON.stringify(res.data.response.user));
-                    localStorage.setItem("token", JSON.stringify(res.data.token));
-                    navigate('/hotels');
-                }
-                else {
-                    setLoginError(res.message);
-                }
+    API({
+      callURL: links.login,
+      callMethod: "POST",
+      bodyData: body,
+      callBack: (res) => {
+        if (res.status) {
+          localStorage.setItem("user", JSON.stringify(res.data.response.user));
+          localStorage.setItem("token", JSON.stringify(res.data.token));
+          navigate("/hotels");
+        } else {
+          setLoginError(res.message);
+        }
+      },
+    });
+  }
 
+  return (
+    <>
+      <form onSubmit={(e) => login(e)}>
+        <div className="mb-3">
+          <label className="form-label">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            value={formData.email}
+            onChange={(e) =>
+              setFormData({ ...formData, email: e.target.value })
             }
-        })
-    }
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            value={formData.password}
+            onChange={(e) =>
+              setFormData({ ...formData, password: e.target.value })
+            }
+          />
+        </div>
+        {loginError !== null && (
+          <p>
+            <font color="red">{loginError}</font>
+          </p>
+        )}
 
-    return (
-        <>
-            <form onSubmit={((e) => login(e))}>
-                <div className="mb-3">
-                    <label className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="email" value={formData.email} onChange={(e) => setFormData({ ...formData, email: e.target.value })} />
-                </div>
-                <div className="mb-3">
-                    <label className="form-label">Password</label>
-                    <input type="password" className="form-control" id="password" value={formData.password} onChange={(e) => setFormData({ ...formData, password: e.target.value })} />
-                </div>
-                {loginError !== null && <p>
-                    <font color='red'>{loginError}</font>
-                </p>}
-
-                <button type="submit" className="btn btn-primary">Login</button>
-                <Link to='/register'>
-                    Register
-                </Link>
-            </form>
-        </>
-    )
+        <button type="submit" className="btn btn-primary">
+          Login
+        </button>
+        <Link to="/register">Register</Link>
+      </form>
+    </>
+  );
 }
 
 export default Login;
